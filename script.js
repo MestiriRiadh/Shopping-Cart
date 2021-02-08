70c53e16c757a197c6f4510adec46189d5904dfd
MestiriRiadh <mestiri.riadh@gmail.com>
2021-02-08 20:56:22 +0100

    discount

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const moin = document.querySelectorAll(".btn-moin");
 const total = document.querySelector(".total-price");
 const remove = document.querySelectorAll(".remove");
 const like = document.querySelectorAll(".like");
-
+const discount = document.querySelector(".discount");
+const code = "riadh";
+let initialtotal;
 for (let i = 0; i < plus.length; i++) {
   plus[i].addEventListener("click", function () {
     plus[i].previousElementSibling.innerHTML++;
@@ -11,6 +13,7 @@ for (let i = 0; i < plus.length; i++) {
     total.innerHTML = (parseFloat(price) + parseFloat(total.innerHTML)).toFixed(
       2
     );
+    initialtotal = total.innerHTML;
   });
 }
 for (let btn of moin) {
@@ -21,6 +24,7 @@ for (let btn of moin) {
       total.innerHTML = (
         parseFloat(total.innerHTML) - parseFloat(price)
       ).toFixed(2);
+      initialtotal = total.innerHTML;
     }
   });
 }
@@ -30,6 +34,14 @@ for (let icon of remove) {
       icon.parentElement.parentElement.parentElement.nextElementSibling.remove();
     }
     icon.parentElement.parentElement.parentElement.remove();
+    let price =
+      icon.parentElement.parentElement.nextElementSibling.lastElementChild
+        .innerHTML;
+    let quantite =
+      icon.parentElement.parentElement.nextElementSibling.firstElementChild
+        .children[1].innerHTML;
+    var res = parseFloat(price) * parseFloat(quantite);
+    total.innerHTML = (total.innerHTML - res).toFixed(2);
   });
 }
 
@@ -38,3 +50,12 @@ for (let btn of like) {
     btn.classList.toggle("like-red");
   });
 }
+discount.addEventListener("keyup", function (e) {
+  let value = e.target.value;
+
+  if (code === value) {
+    total.innerHTML = total.innerHTML - total.innerHTML * 0.2;
+  } else {
+    total.innerHTML = initialtotal;
+  }
+});
